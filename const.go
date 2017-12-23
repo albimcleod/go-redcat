507af46e7a9e68f65e95810ee97cc1257901eb96
albimcleod <albimcleod@gmail.com>
2017-12-24 08:06:25 +1030

    update consts

--- const.go
+++ const.go
@@ -1,15 +1,16 @@
 package goredcat
 
 const (
-	FieldHeaderID     = "HeaderID"
-	FieldLineID       = "LineID"
-	FieldCategoryID   = "CategoryID"
-	FieldCategoryName = "CategoryName"
-	FieldPrice        = "Price"
-	FieldQty          = "Sold"
-	FieldGST          = "GST"
-	FieldAmount       = "Amount"
-	FieldDate         = "TxnDateTime"
-	FieldStore        = "StoreID"
-	FieldSaleID       = "SaleID"
+	FieldHeaderID           = "HeaderID"
+	FieldLineID             = "LineID"
+	FieldCategoryID         = "CategoryID"
+	FieldCategoryName       = "CategoryName"
+	FieldPrice              = "Price"
+	FieldQty                = "Sold"
+	FieldGST                = "GST"
+	FieldAmount             = "Amount"
+	FieldDate               = "TxnDateTime"
+	FieldStore              = "StoreID"
+	FieldSaleID             = "SaleID"
+	FieldLastUpdateDateTime = "LastUpdateDateTime"
 )
